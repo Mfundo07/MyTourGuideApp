82e8a8510631ac1c570da3e56499d1d9dfc9e180
Mfundo07 <mfundombokoma07@gmail.com>
2017-08-03 16:05:33 +0200

    commit

--- app/src/main/java/com/example/android/mytourguideapp/MainActivity.java
+++ app/src/main/java/com/example/android/mytourguideapp/MainActivity.java
@@ -1,26 +1,68 @@
 package com.example.android.mytourguideapp;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button viewData;
+    private  Button tourGuide;
+    private Button eatData;
+    private Button drinkData;
+    private Button stayData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewData = (Button) findViewById(R.id.view_data);
+        tourGuide = (Button) findViewById(R.id.tour_guide);
+        eatData = (Button) findViewById(R.id.eat_data);
+        drinkData = (Button) findViewById(R.id.drink_data);
+        stayData = (Button) findViewById(R.id.stay_data);
+        viewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ViewsTourDataActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager(),this);
+        tourGuide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TourGuideActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        viewPager.setAdapter(adapter);
+        eatData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EatTourDataActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        drinkData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(MainActivity.this,DrinkTourDataActivity.class);
+                startActivity(intent);
+            }
+        });
+        stayData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,StayTourDataActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
-        tabLayout.setupWithViewPager(viewPager);
     }
 }
